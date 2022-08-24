be1ebc04b85b45c4b831affa9cc4d9a05d58fee8
Dzmitry <78901012+andrqn@users.noreply.github.com>
2022-08-24 16:36:50 +0300

    Update 1.03.php

--- 1.03.php
+++ 1.03.php
@@ -1,30 +1,21 @@
 <?php
 /*Выяснить, образуют ли цифры данного натурального числа N возрастающую последовательность.*/
 
-$num = 5;
-$res = 0 ;
-for ($i = 1000; $i < 10000; $i++)
-{
-    $firstNum = ($i - ($i % 1000)) / 1000;
-    $secondNum = (($i % 1000) - ($i % 100)) / 100;
-    $thirdNum = (($i % 100) - $i % 10) / 10;
-    $lastNum = $i % 10;
-    $sumNum = $firstNum + $secondNum + $thirdNum + $lastNum;
-    if ($num == $sumNum)
-    {
-        if (($firstNum < $secondNum) && ($secondNum < $thirdNum) && ($thirdNum < $lastNum))
-        {
-            $res+=1;
-        }
-
+$num = 1234;
+$numCheck = $num % 10;
+$lastNum = 0;
+$flag = true;
+while ($num != 0) {
+    $lastNum = $num % 10;
+    if ($numCheck < $lastNum) {
+        $flag = false;
+    } else {
+        $numCheck = $lastNum;
     }
-
+    $num = intdiv($num, 10);
 }
-if ($res > 0)
-{
-    echo "Цифры данного натурального числа $num образуют возрастающую последовательность.";
-}
-else
-{
-    echo "Цифры данного натурального числа $num не образуют возрастающую последовательность.";
+if ($flag) {
+    echo "Цифры данного натурального числа  образуют возрастающую последовательность.";
+} else {
+    echo "Цифры данного натурального числа  не образуют возрастающую последовательность.";
 }
