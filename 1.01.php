53af43343bff87463206b05303ddd175c0fc038c
Dzmitry <78901012+andrqn@users.noreply.github.com>
2022-08-24 16:47:40 +0300

    Update 1.01.php

--- 1.01.php
+++ 1.01.php
@@ -1,24 +1,13 @@
 <?php
 /*Определить  количество  цифр,  меньших  5,  используемых  при  записи натурального числа N.*/
-$num = 1255;
-$firstNum = ($num - ($num % 1000)) / 1000;
-$secondNum = (($num % 1000) - ($num % 100)) / 100;
-$thirdNum = (($num % 100) - $num % 10) / 10;
-$lastNum = $num % 10;
+
+$num = 12333;
 $countNum = 0;
-if ($firstNum < 5) {
-    $countNum++;
+while ($num != 0) {
+    $lastNum = $num % 10;
+    if ($lastNum < 5) {
+        $countNum++;
+    }
+    $num = intdiv($num, 10);
 }
-if ($secondNum < 5) {
-    $countNum++;
-}
-if ($thirdNum < 5) {
-    $countNum++;
-}
-if ($lastNum < 5) {
-    $countNum++;
-}
-
 echo "Чисел меньше 5 : $countNum";
-
-?>
