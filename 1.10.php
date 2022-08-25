5eb26d46ad1ae5f17ed1c9c17b9c59b1a6866d4e
Dzmitry <78901012+andrqn@users.noreply.github.com>
2022-08-25 15:32:12 +0300

    Update 1.10.php

--- 1.10.php
+++ 1.10.php
@@ -2,11 +2,22 @@
 /*Найти все меньшие N числа-палиндромы, которые при возведении в квадрат дают палиндром. Число называется палиндромом,
 если его запись читается одинаково с начала и с конца.*/
 
-$num = 100;
+$numCheck = 100;
 
-for ($i=10;$i<$num;$i++)
+for ($i=10;$i<$numCheck;$i++)
 {
-    if (($i==strrev($i))&&($i*$i==strrev($i*$i))){
+
+    $num=$i;
+    $newNum=0;
+    while ($num != 0)
+    {
+        $lastNum = $num % 10;
+        $newNum = ($newNum * 10) + $lastNum;
+        $num = intdiv($num, 10);
+
+    }
+    
+    if (($i==$newNum)&&(($i*$i)==($newNum*$newNum))){
         echo "$i\n";
     }
 
