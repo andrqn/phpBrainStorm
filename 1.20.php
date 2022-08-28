11d217db50032135998cf15ff617e0c4a72174a4
Dzmitry <78901012+andrqn@users.noreply.github.com>
2022-08-28 10:06:36 +0300

    Update 1.20.php

--- 1.20.php
+++ 1.20.php
@@ -1,11 +1,15 @@
 <?php
 
 /*Найти целое число в диапазоне от N до M с наибольшей суммой  делителей.  */
+
 $numStart = 6;
 $numStop = 32;
 $sumMax = 0;
-for ($i = $numStart; $i <$numStop; $i++) {
+
+for ($i = $numStart; $i <$numStop; $i++) 
+{
     $sum = 0;
+    
     for ($j = 1; $j < $i; $j++) {
         if ($i % $j == 0) {
             $sum += $j;
