cbc2ae3b0f458560107df0da1fac95b216ed5570
Dzmitry <78901012+andrqn@users.noreply.github.com>
2022-08-28 11:56:40 +0300

    Add files via upload

---
+++ 1.26.php
@@ -0,0 +1,23 @@
+<?php
+
+/*Среди натуральных чисел  n0 ,n1,...,nm найти число с максимальной  суммой делителей.  */
+$numStart = 1;
+$numStop = 24;
+$max = 0;
+$sum = 0;
+for ($i = $numStart; $i < $numStop; $i++)
+{
+    $num = $i;
+    for ($j = 1; $j < $num; $j++)
+    {
+        if ($i % $j == 0) {
+            $sum += $j;
+            echo "$sum\n";
+        }
+    }
+    if ($sum >= $max) {
+        $max = $sum;
+    }
+    $sum = 0;
+}
+echo $max;
